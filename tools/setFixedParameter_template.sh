2fd5a63711a89c6be39794e5ff5de34d2c2e267d
Ayla Nawaz <anawaz@max-wgse002.desy.de>
2020-01-10 11:19:33 +0100

    setFixedParameter.sh added to .gitignore, created setFixedParameter_template.sh

---
+++ tools/setFixedParameter_template.sh
@@ -0,0 +1,6 @@
+dataDirectory=/beegfs/desy/user/jhktimm/rawdata/                               # !!!!!!!!!!!!!!!
+resultDirectory=/beegfs/desy/user/jhktimm/results/                               # !!!!!!!!!!!!!!!
+aoaDirectory=/home/jhktimm/sources/aoa_delta/                          # !!!!!!!!!!!!!!!
+logDirectory=/beegfs/desy/user/jhktimm/logs/                           # !!!!!!!!!!!!!!!
+
+userMail=jhktimm@mail.desy.de
