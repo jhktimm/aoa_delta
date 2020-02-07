39b9e05588217fc6d9d4d9c9c98b79aeaf1e5d83
Ayla Nawaz <anawaz@max-wgs001.desy.de>
2020-02-07 15:46:00 +0100

    removal of Noether Parameters in /tau_k_x

---
+++ tools/maxMerge_Mona_Catwright.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH --mail-type ALL                           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user jhktimm@mail.desy.de          # Email to which notifications will be sent. It defaults to <userid@mail.desy.de> if none is set.
+#SBATCH --time    0-04:00:00
+#SBATCH --nodes            1
+#SBATCH --partition maxwell
+#SBATCH --no-requeue                                               # !!!!!!!!!!!!!!!
+#SBATCH --job-name maXfelMerge
+#SBATCH --output    job-%x-%A-%a-%j-%N.out
+#SBATCH --error     job-%x-%A-%a-%j-%N.err            # File to which STDERR will be written
+
+function nrwait() {
+  local nrwait_my_arg
+  if [[ -z $1 ]] ; then
+    nrwait_my_arg=$(grep "processor" < /proc/cpuinfo | wc -l)
+#      nrwait_my_arg=2
+  else
+    nrwait_my_arg=$1
+  fi
+
+  while [[ $(jobs -p | wc -l) -ge $nrwait_my_arg ]] ; do
+    sleep 0.33;
+  done
+}
+
+resultDirectory=/beegfs/desy/user/anawaz/merged/
+dataToMergeDirectory=/beegfs/desy/user/aeichler/results/
+name=Mona_Catwright
+
+for station in A17 A12; do
+  for module in {1..4} ; do
+    for cavity in {1..8} ; do
+      echo ${resultDirectory}_${name}${station}.L3.M${module}.C${cavity}.dat  ${dataToMergeDirectory}*.L3.M${module}.C${cavity}_${name}_*.dat
+      ./mergeDat.sh  ${resultDirectory}_${name}${station}.L3.M${module}.C${cavity}.dat  ${dataToMergeDirectory}*.L3.M${module}.C${cavity}_${name}_*.dat &
+      echo `date` `jobs -l`
+      nrwait
+    done
+  done
+done
+
+jobs -l
+
+wait
+echo `date` jobs are finnish
